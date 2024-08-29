87d7169e0d98c5def36f18035b5eb017db5e0d4e
Jano Personal <esterhuizenjano@gmail.com>
2024-08-29 10:13:17 +0200

    Added Product CRUD opperations Created Local DB

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
